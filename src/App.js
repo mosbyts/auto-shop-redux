0724fd88780d503a4fe9ffd7259cf2106c5e92ba
Tianna Mosby <mosbyts@vcu.edu>
2020-03-14 22:47:05 -0400

    Basic CRA starter files.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>App Component</h1>
+      {/* Remove the contents of the component and add rendering logic here */}
+    </div>
+  );
+}
