0f8821826fe9801af3037b5676e01a446df4e11e
Tianna Mosby <mosbyts@vcu.edu>
2020-03-16 19:09:38 -0400

    Updates

---
+++ precache-manifest.3c256dff403dc46304bc4e785cf18155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16e921d29da83cb1303e",
+    "url": "/auto-shop-redux/static/js/runtime~main.592bc26e.js"
+  },
+  {
+    "revision": "2c5242a8b2fd21159031",
+    "url": "/auto-shop-redux/static/js/main.dce0e61b.chunk.js"
+  },
+  {
+    "revision": "1e792e6d416f53d07156",
+    "url": "/auto-shop-redux/static/js/2.31147a52.chunk.js"
+  },
+  {
+    "revision": "2c5242a8b2fd21159031",
+    "url": "/auto-shop-redux/static/css/main.272606f1.chunk.css"
+  },
+  {
+    "revision": "8d52fbbd3f841de534d0002033c77231",
+    "url": "/auto-shop-redux/index.html"
+  }
+];
